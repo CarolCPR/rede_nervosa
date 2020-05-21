b6cb9b37a00cf37546ca7d1e1bf4e4f3d3a9592b
Carol da Rosa <caroolpach@outlook.com>
2020-05-20 21:58:45 -0300

    rollback

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ var app = express();
 /*app.get('/', function (req, res) {
   res.send('Hello World!');
 });*/
-app.use((req, res, next) => { //Cria um middleware onde todas as requests passam por ele 
+/*app.use((req, res, next) => { //Cria um middleware onde todas as requests passam por ele 
   if (req.headers["x-forwarded-proto"] == "http") //Checa se o protocolo informado nos headers é HTTP 
       res.redirect(`https://${req.headers.host}${req.url}`); //Redireciona pra HTTPS 
   else //Se a requisição já é HTTPS 
       next(); //Não precisa redirecionar, passa para os próximos middlewares que servirão com o conteúdo desejado 
-});
+});*/
 app.use(express.static('public'));
 
 app.listen(3000, function () {
